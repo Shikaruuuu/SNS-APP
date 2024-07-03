81fe5ad2125fd68274f148128835a72693f2a37b
Shikaruuuu <horuzou17@gmail.com>
2024-07-04 02:53:47 +0900

    remove unnecessary images

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -6,46 +6,13 @@ export default function Rightbar({ user }) {
   const HomeRightbar = () => {
     return (
       <>
-        {/* <p className="promotionTitle">プロモーション広告</p>
-        <img src="assets/promotion/promotion1.jpeg" alt=""  className="rightbarPromotionImg" />
-        <p className="promotionName">ショッピング</p>
-        <img src="assets/promotion/promotion2.jpeg" alt=""  className="rightbarPromotionImg" />
-        <p className="promotionName">カーショップ</p>
-        <img src="assets/promotion/promotion3.jpeg" alt=""  className="rightbarPromotionImg" />
-        <p className="promotionName">モーリーワークス</p> */}
-        </>
+      </>
     );
   };
 
   const ProfileRightbar = () => {
     return (
       <>
-      {/* <h4 className="rightbarTitle">ユーザー情報</h4>
-      <div className="rightbarInfo">
-        <div className="rightbarInfoItem">
-          <span className="rightbarInfoKey">出身：</span>
-          <span className="rightbarInfoKey">福岡</span>
-        </div>
-        <h4 className="rightbarTitle">あなたの友達</h4>
-        <div className="rightbarfollowings">
-          <div className="rightbarFollowing">
-            <img src={PUBLIC_FOLDER + "/person/1.jpeg"} alt="" className="rightbarFollowingImg" />
-            <span className="rightbarFollowingName">れにゃあ</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img src={PUBLIC_FOLDER + "/person/2.jpeg"} alt="" className="rightbarFollowingImg" />
-            <span className="rightbarFollowingName">yamaki</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img src={PUBLIC_FOLDER + "/person/3.jpeg"} alt="" className="rightbarFollowingImg" />
-            <span className="rightbarFollowingName">koga</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img src={PUBLIC_FOLDER + "/person/4.jpeg"} alt="" className="rightbarFollowingImg" />
-            <span className="rightbarFollowingName">matsukubo</span>
-          </div>
-        </div>
-      </div> */}
       </>
     );
   };
